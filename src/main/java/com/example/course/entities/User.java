79c129addd0583fa0f7a33e70229e3117b19fec2
nicoleb30 <nicoleb3007@gmail.com>
2024-02-20 09:26:22 -0300

    criacao do projeto/ camadas da classe User

---
+++ src/main/java/com/example/course/entities/User.java
@@ -0,0 +1,86 @@
+package com.example.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb-user")
+public class User implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	
+	
+	public User() {
+		
+	}
+	
+	
+	public User(Long id, String name, String email, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Long getId() {
+		return id;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	} 
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.example.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.entities.User;
+import com.example.course.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findall();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+}
