a36dcc3b304e130ea8ebd9bd8d91a265d2e6ef79
nicoleb30 <nicoleb3007@gmail.com>
2024-02-20 19:25:02 -0300

    orderStatus enum

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.entities.enums.OrderStatus;
+import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
 @Configuration
@@ -17,12 +21,21 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "maria", "maria@gmail.com", "47988210669");
 		User u2 = new User(null, "joao", "joao@gmail.com", "47987654321");
 		
+		Order o1 = new Order(null, Instant.parse("2024-01-20T19:53:07Z"), OrderStatus.PAID,  u1);
+		Order o2 = new Order(null, Instant.parse("2024-02-21T03:42:10Z"),OrderStatus.WAITING_PAIMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2024-02-22T15:21:22Z"), OrderStatus.WAITING_PAIMENT, u1);
+		
+		
 		repository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
 	}
 
--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -1,9 +1,10 @@
 package com.example.course.entities;
 
-
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
+
+import com.example.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,16 +16,17 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb-order")
-public class Order implements Serializable{
-
+public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
-	private Long id; 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private Instant moment;
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,10 +35,11 @@ public class Order implements Serializable{
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -46,6 +49,15 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus!= null)
+		this.orderStatus = orderStatus.getCode();
 	}
 
 	public User getClient() {
@@ -75,9 +87,6 @@ public class Order implements Serializable{
 			return false;
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
-	} 
-	
-	
-	
-	
+	}
+
 }
