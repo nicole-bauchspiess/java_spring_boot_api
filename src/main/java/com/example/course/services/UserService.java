856e1288d05d178dd50d03ee01fb36daecf706d4
nicoleb30 <nicoleb3007@gmail.com>
2024-02-20 09:50:30 -0300

    user findById

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,9 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	public User findbyId(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
 	
 }
