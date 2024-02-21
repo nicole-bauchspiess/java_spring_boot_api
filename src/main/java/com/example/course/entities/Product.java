35bad23702578f1a74446bce02411622cb420040
nicoleb30 <nicoleb3007@gmail.com>
2024-02-20 23:14:15 -0300

    many to many products- orderItem

--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,6 +31,11 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
+	
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	
 	@ManyToMany
 	@JoinTable(name = "tb-prodct-category", joinColumns = @JoinColumn(name = "product_id"),
@@ -47,6 +55,15 @@ public class Product implements Serializable{
 		this.imgUrl = imgUrl;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem value: items) {
+			set.add(value.getOrder());
+		}
+		return set;
+	}
+	
 	public Long getId() {
 		return id;
 	}
